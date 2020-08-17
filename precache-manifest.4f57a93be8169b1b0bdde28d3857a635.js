8d4938b69a21ec523eadef3ed7c18a6b7eec237f
pragatib <bhandariprag7@gmail.com>
2020-08-17 07:26:13 +0300

    Updates

---
+++ precache-manifest.4f57a93be8169b1b0bdde28d3857a635.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85cbcae3e10042ca67e38f0b2beb795e",
+    "url": "/ShoppingListApp/index.html"
+  },
+  {
+    "revision": "d89cd49e9116c67ec507",
+    "url": "/ShoppingListApp/static/js/2.d12651ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShoppingListApp/static/js/2.d12651ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a7f1f9983345e52a2dc",
+    "url": "/ShoppingListApp/static/js/main.242cbf40.chunk.js"
+  },
+  {
+    "revision": "48b6d47fc2754f88f44e",
+    "url": "/ShoppingListApp/static/js/runtime-main.4ec3411c.js"
+  }
+]);
